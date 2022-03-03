6c09dfc6487db18ec58a39939791abbe0772d381
= <birchn@byui.edu>
2022-03-02 18:39:37 -0700

    finished stretch challenge

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
 const professionalRoutes = require('./routes/professional');
 
+const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(bodyParser.json());
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/professional', professionalRoutes);
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
-
-app.use('/professional', professionalRoutes);
-
-app.listen(8080);
