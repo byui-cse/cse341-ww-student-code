4f61e5813c7e88eface3fb01d58b1a7fc82111a0
= <birchn@byui.edu>
2022-03-04 12:20:02 -0700

    removing unused variable

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const swaggerAutogen = require('swagger-autogen')();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
