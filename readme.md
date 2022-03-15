f7649959b452659ee92e7b039eb10a08f3e3a276
Nathan Birch <birchn@byui.edu>
2022-03-15 09:03:16 -0600

    completed code for class activity

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Lesson 8 Class Solution
+
+This is the completed collection of code for the lesson 8 class activity
+
+## Video
+
+[Video walkthrough](https://youtu.be/52gurOGtaJM)
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const { auth, requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
